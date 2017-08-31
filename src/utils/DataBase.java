565411028e8a40ed959ed7a5b5809c541fac0aa7
Alumno12 <Alumno12@192.168.1.51>
2017-08-31 12:55:54 +0200

    database solved

--- src/utils/DataBase.java
+++ src/utils/DataBase.java
@@ -3,18 +3,25 @@ package utils;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
 public class DataBase {
+	
+	public final static int INTEGER = 0;
+	public final static int STRING = 1;
+	public final static int FLOAT = 2;
+	
 	private final static String URL = "jdbc:mysql://localhost:3306/portal";
 	private final static String USER = "root";
 	private final static String PASS = "test1234";
 	
 	private static Connection conn;
 	
-	public static Connection getConnector() {
+	private void init() {
 		if(conn==null) {
 			try {
 				conn = (Connection) DriverManager.getConnection(URL, USER, PASS);
@@ -22,27 +29,55 @@ public class DataBase {
 			    throw new IllegalStateException("Cannot connect the database!", e);
 			}
 		}
-		return conn;
 	}
 	
-	public static ResultSet get(String query) throws SQLException {
-		Statement stmt = (Statement) conn.createStatement();
-		ResultSet result = stmt.executeQuery(query);
-		stmt.close();
-		return result;
-	}
-	
-	public static void update(String query) throws SQLException {
-		Statement stmt = (Statement) conn.createStatement();
-		stmt.execute(query);
-		stmt.close();
-	}
-	
-	public static void close() throws SQLException {
+	private void close() throws SQLException {
 		if(conn!=null) {
 			conn.close();
 			conn = null;
 		}
+	}
+	
+	public ResultSet get(String sql,List<BindValue> bind) throws SQLException {
+		PreparedStatement prep = preparaquery(sql,bind);
+		ResultSet result = prep.executeQuery();
+		prep.close();
+		close();
+		return result;
+	}
+	
+	public int update(String sql,List<BindValue> bind) throws SQLException {
+		PreparedStatement prep = preparaquery(sql,bind);
+		int result = prep.executeUpdate();
+		prep.close();
+		close();
+		return result;
+	}
+	
+	private PreparedStatement preparaquery(String sql,List<BindValue> bind) throws SQLException {
+		init();
+		PreparedStatement prep = (PreparedStatement) conn.prepareStatement(sql);
+		for(int i=0;i<bind.size();i++) {
+			BindValue val = bind.get(i);
+			switch(val.getType()) {
+				case INTEGER:
+					prep.setInt(i,Integer.parseInt(val.getValue()));
+					break;
+				case STRING:
+					prep.setString(i,val.getValue());
+					break;
+				case FLOAT:
+					prep.setFloat(i,Float.parseFloat(val.getValue()));
+					break;
+			}
+		}
+		return prep;
+	}
+	
+	
+	public interface BindValue{
+		public int getType();
+		public String getValue();
 	}
 	
 }
